3597e9368c436b5aff8cf2b79f9fef3181520051
boom <boom@pkmn.gold>
2023-11-06 12:13:05 -0500

    initial upload

---
+++ 5e-HP-calculator.py
@@ -0,0 +1,15 @@
+name = "Lee"
+
+level = 7 
+# replace with level
+
+conmod = 4 
+# replace with Consitution modifier
+
+barbHP = (12 + conmod) + ((7 + conmod) * (level - 1))
+# calculation for barbarian 5e HP - using averages, not rolls
+
+status = "very tiny and cute."
+
+
+print(name, "is level", level, "and has", barbHP , "health points. They are, also,", status)
