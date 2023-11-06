012f119ac95515107ebb225d5ca78b029a52a9dd
boom <boom@pkmn.gold>
2023-11-06 13:15:55 -0500

    clean-up

--- 5e-HP-calculator.py
+++ 5e-HP-calculator.py
@@ -1,18 +1,19 @@
-name = "Lee"
+name = "Lee" # character's name, Lee by default
+level = 7 # replace with level
+conmod = 2 # replace with Consitution modifier
+barbHP = (12 + conmod) + ((7 + conmod) * (level - 1)) # calculation for barbarian 5e HP (averaged)
+status = "very tiny and cute." # just fluff to make wifey smile
 
-level = 7
-# replace with level
+###
 
-conmod = 2 
-# replace with Consitution modifier
+print(name, "is level", level, "and has", barbHP , "HP. They are, also,", status)
+# HP calculation for a 5e barbarian without feats or racial bonuses
 
-barbHP = (12 + conmod) + ((7 + conmod) * (level - 1))
-# calculation for barbarian 5e HP - using averages, not rolls
+print("If", name, "is a Hill Dwarf, their HP is", barbHP + level, "instead - nice beard.")
+# HP calculation for a 5e barbarian with the Hill Dwarf racial bonus
 
-status = "very tiny and cute."
+print("If", name, "has the Tough feat, they have", barbHP + (level * 2), "HP and sick muscles.")
+# HP calculation for a 5e barbarian with Tough feat
 
-
-print(name, "is level", level, "and has", barbHP , "health points. They are, also,", status)
-print("If", name, "has the Tough feat, their health points become", barbHP + (level * 2))
-print("If", name, "is a Hill Dwarf, their health points are", barbHP + level, "instead.")
-print("If", name, "is a Tough Hill Dwarf, their health points are", barbHP + level + (level * 2))
+print("If", name, "is a Tough Hill Dwarf, their HP is", barbHP + level + (level * 2), "and they should be the party's tank.")
+# HP calculation for a 5e barbarian with both Hill Dwarf and Tough bonuses
